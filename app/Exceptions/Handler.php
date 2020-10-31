28b4fc9b05731817f3b0d5a6e834efbf9bc05d27
Mohamed Keita <mohamed.keita@hakili-corp.com>
2020-10-31 11:27:42 +0100

    update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        var_dump($exception->getTraceAsString());exit;
         return response()->json(
             [
                 'status' => 'error',
