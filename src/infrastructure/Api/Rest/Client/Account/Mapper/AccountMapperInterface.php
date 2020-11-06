b0509f6bf7f93d648990140237ebd6ee0bff8a34
Mohamed Keita <mohamed.keita@hakili-corp.com>
2020-11-06 23:08:33 +0100

    update

--- src/infrastructure/Api/Rest/Client/Account/Mapper/AccountMapperInterface.php
+++ src/infrastructure/Api/Rest/Client/Account/Mapper/AccountMapperInterface.php
@@ -5,7 +5,7 @@ namespace Infrastructure\Api\Rest\Client\Account\Mapper;
 
 
 use Psr\Http\Message\ResponseInterface;
-use Wallet\Account\Entity\AccountEntityInterface;
+use Wallet\Wallet\Account\Entity\AccountEntityInterface;
 use Wallet\Wallet\Account\Collection\AccountCollectionInterface;
 
 interface AccountMapperInterface
